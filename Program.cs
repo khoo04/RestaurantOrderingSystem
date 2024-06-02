7a2843a8b3afce0e30096fb0073b995bc05d766b
Khoo Zhen Xian <khoozhenxian@gmail.com>
2024-06-02 23:19:31 +0800

    Changes in Form

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace RestaurantOrderingSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm()) ;
+            Application.Run(new MainForm()) ;
         }
     }
 }
